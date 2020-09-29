3ff0fa819cc3415521dab81f184f123670ef50dd
reeglyson <reeglysonmescabal@gmail.com>
2020-09-30 01:55:05 +0800

    added enuD that enumerates a dictionary

--- py_notes.py
+++ py_notes.py
@@ -9,11 +9,12 @@ def enu(input_array):
             print(count, entry)
             count += 1
 
-def enuT(input_array, r_resp, enable_debug = True):
-        ''' given input_array=dir(r) and r_resp = r '''
+def enuT(input_class, enable_debug = True):
+        ''' given a class, list all entries within dir(<class>) then tell me if its a method or attribute '''
         count = 0
-        r = r_resp
+        r = input_class
         tempo_array = []
+        input_array = dir(input_class)
         for entry in input_array:
                 tempo_string = 'type(r.'+entry+')'
                 tempo_tup = (count, entry, eval(tempo_string))
@@ -22,5 +23,16 @@ def enuT(input_array, r_resp, enable_debug = True):
                 count += 1
 
         return tempo_array
-
+		
+def enuD(input_dict, enable_debug=True):
+	''' enumerate keys and values for a given dictionary '''
+	count = 0
+	tempo_array = []
+	for key in input_dict.keys():
+		tempo_tuple = (count, key, input_dict[key])
+		print(tempo_tuple)
+		tempo_array.append(tempo_tuple)
+		count += 1
+	return tempo_array
+	
 
